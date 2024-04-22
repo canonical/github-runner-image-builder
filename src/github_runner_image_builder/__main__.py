1147f5902e2b82a14063c08c17f3ab2d17615683
charlie4284 <yangsoo.yoon@canonical.com>
2024-04-22 15:35:28 +0900

    rename dir

---
+++ src/github_runner_image_builder/__main__.py
@@ -0,0 +1,18 @@
+"""liquidctl – monitor and control liquid coolers and other devices.
+
+Copyright Jonas Malaco and contributors
+SPDX-License-Identifier: GPL-3.0-or-later
+"""
+
+# uses the psf/black style
+
+import sys
+
+
+def main():
+    """The main entrypoint for the executable."""
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
