1d87298712466420cfc1ac97d7b6a6252c7303e1
Yanks Yoon <yangsoo.yoon@canonical.com>
2024-08-20 11:07:56 +0000

    add logging

---
+++ src/github_runner_image_builder/logging.py
@@ -0,0 +1,26 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Configure logging for GitHub runner image builder."""
+
+import logging
+import logging.handlers
+import pathlib
+
+LOG_FILE_DIR = pathlib.Path("/var/log/github-runner-image-builer/")
+LOG_FILE_PATH = LOG_FILE_DIR / "info.log"
+ERROR_LOG_FILE_PATH = LOG_FILE_DIR / "error.log"
+
+
+def configure(log_level: str | int):
+    """Configure the global log configurations."""
+    log_handler = logging.handlers.WatchedFileHandler(filename=LOG_FILE_PATH, encoding="utf-8")
+    log_handler.setLevel(log_level)
+    error_log_handler = logging.handlers.WatchedFileHandler(
+        filename=ERROR_LOG_FILE_PATH, encoding="utf-8"
+    )
+    logging.basicConfig(
+        level=log_level,
+        handlers=(log_handler, error_log_handler),
+        encoding="utf-8",
+    )
